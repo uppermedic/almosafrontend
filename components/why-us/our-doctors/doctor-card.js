c8ac9e3fd97b7a14b1e4f1224e2e7a955e70aaef
MahmoudAbdullh <ma7moud3bd2llh@hotmail.com>
2021-09-04 14:39:38 +0200

    update doctors

--- components/why-us/our-doctors/doctor-card.js
+++ components/why-us/our-doctors/doctor-card.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { i18n } from 'root/i18n';
+import Link from 'next/link';
 
 const doctor_card = ({ doctor, setcurrentDoctor }) => {
   const { language } = i18n;
@@ -12,13 +13,15 @@ const doctor_card = ({ doctor, setcurrentDoctor }) => {
     }
   };
   return (
-    <div className="card" onClick={handleClick}>
-      <img src={doctor.image} alt={language && doctor[language].name} />
-      <div className="banner">
-        <h4>{language && doctor[language].name}</h4>
-        <p>{language && doctor[language].title}</p>
-      </div>
-    </div>
+    <Link href={`/our-doctors/${doctor.slug}`}>
+      <a className="card" onClick={handleClick}>
+        <img src={doctor.image} alt={language && doctor[language].name} />
+        <div className="banner">
+          <h4>{language && doctor[language].name}</h4>
+          <p>{language && doctor[language].title}</p>
+        </div>
+      </a>
+    </Link>
   );
 };
 
--- components/why-us/our-doctors/index.js
+++ components/why-us/our-doctors/index.js
@@ -39,77 +39,8 @@ const index = ({
   }, [data]);
   return (
     <section className="our-doctors">
-      <Row className="doctor-details m-auto">
-        {/*  the comming data will come when create main doctor key and value  */}
-        <Col lg={4} className="doctor-img">
-          <img
-            src={currentDoctor?.image}
-            alt={language && currentDoctor && currentDoctor[language]?.name}
-            className="shadow"
-          />
-
-          {/* <div className="doctor-social">
-            <Link href="#">
-              <i className="fab fa-linkedin fa-3x"></i>
-            </Link>
-            <Link href="#">
-              <i className="fab fa-twitter-square fa-3x"></i>
-            </Link>
-            <Link href="#">
-              <i className="fas fa-link fa-3x"></i>
-            </Link>
-          </div> */}
-        </Col>
-        <Col lg={{ size: 8 }} className="pl-sm-5 pr-4 ">
-          <h3 className="doctor-name">
-            {language && currentDoctor && currentDoctor[language]?.name}
-          </h3>
-          <p className="py-2 doctor-title">
-            {language && currentDoctor && currentDoctor[language]?.title}
-          </p>
-          {language &&
-            currentDoctor &&
-            currentDoctor[language].qualifications &&
-            currentDoctor[language].qualifications.length > 0 && (
-              <>
-                <h3 className="doctor-qualifications">
-                  {t('common:qualifications')}
-                </h3>
-
-                <Markdown>
-                  {language
-                    ? strippedContent(currentDoctor[language].qualifications)
-                    : ''}
-                </Markdown>
-              </>
-            )}
-          {language &&
-            currentDoctor &&
-            currentDoctor[language].current_positions &&
-            currentDoctor[language].current_positions.length > 0 && (
-              <>
-                <h3 className="doctor-position">
-                  {t('common:current_positions')}
-                </h3>
-                <Markdown>
-                  {language && currentDoctor
-                    ? strippedContent(
-                        currentDoctor[language].current_positions || ''
-                      )
-                    : ''}
-                </Markdown>
-              </>
-            )}
-        </Col>
-      </Row>
-      <Row>
-        <Col className="pt-5 ">
-          <h2>{t('menu:our doctors')}</h2>
-        </Col>
-      </Row>
-
       <Row xs="1" sm="2" md="3" lg="4" className="doctors-cards">
-        {data.doctors.data.map((doctor, index) => (
+        {data.our_doctors.data.map((doctor, index) => (
           <Col>
             <DoctorCard
               key={index}
