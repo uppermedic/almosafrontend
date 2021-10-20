919830d1804c6016c445170ad156308c8684156a
Randa Magdy <randa@innovation-hub.ltd>
2021-10-20 23:40:24 +0300

    -make carousal in home to autoplay in small screens & fix some styles on it

--- components/Home/CarouselContent.js
+++ components/Home/CarouselContent.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Button } from 'reactstrap';
+import React from 'react';
 import { i18n, Link, withTranslation } from 'root/i18n';
 import { strippedContent } from 'src/utils/helpers';
 import Markdown from 'markdown-to-jsx';
@@ -10,11 +9,6 @@ import {
 
 function CarouselContent({ data, index, t }) {
   const { language } = i18n;
-  const [locale, setlocale] = useState('');
-
-  useEffect(() => {
-    setlocale(language);
-  }, [language]);
 
   if (data) {
     return (
